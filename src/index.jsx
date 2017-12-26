1ca014886dee6432a2dfde19465c3f58975331d2
Nikita Chernyshov <xpom55@mail.ru>
2017-12-26 19:23:35 +0300

    added hmr but NOT WORKING =)

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-  <h1>Hello, world!222</h1>,
-  document.getElementById('root')
-);
+import App from './App';
+
+const render = () => {
+  ReactDOM.render(<App />, document.getElementById('root'));
+};
+
+render();
+
+if (module.hot) {
+  console.log('Update');
+  module.hot.accept('./App', function() {
+    console.log('Accepting the updated printMe module!');
+    render();
+  });
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,29 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // создание кастомных html файлов
+const CopyWebpackPlugin = require('copy-webpack-plugin'); // копирование файлов
+const CleanWebpackPlugin = require('clean-webpack-plugin'); // удаление файлов
+const webpack = require('webpack'); // для HMR
 
 const PATHS = {
-  source: path.join(__dirname, 'src'),
-  build: path.join(__dirname, 'build')
+  source: path.join(__dirname, 'src'), // основной путь к файлам
+  build: path.join(__dirname, 'build') // пусть к билду
 };
 
 module.exports = {
-  entry: PATHS.source + '/index.jsx',
+  entry: [
+    'react-hot-loader/patch', // HMR
+    PATHS.source + '/index.jsx', // точка входа
+  ],
   output: {
-    path: PATHS.build,
-    filename: '[name].js'
+    path: PATHS.build, // точка выгрузки файлов
+    filename: '[name].bundle.js' // создаем бандлы по имени
   },
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
-        include: [path.resolve(__dirname, 'src')],
-        exclude: [path.resolve(__dirname, 'node_modules')],
+        test: /\.jsx?$/, // обработка jsx? файлов
+        include: [path.resolve(__dirname, 'src')], // включаем файлы для обработки
+        exclude: [path.resolve(__dirname, 'node_modules')], // исключаем файлы из обработки
         loader: 'babel-loader',
         options: {
           presets: ['es2015', 'react']
@@ -26,10 +32,31 @@ module.exports = {
     ]
   },
   plugins: [
+    new CopyWebpackPlugin([
+      {
+        from: './static/' // копируем папку static в папку build
+      }
+    ]),
     new HtmlWebpackPlugin({
-      title: 'hello nikita!!!2 =)',
-      template: require('html-webpack-template'),
-      bodyHtmlSnippet: '<div id="root"></div>'
-    })
-  ]
+      title: 'CustomTitle',
+      template: require('html-webpack-template'), // плагин нужен для кастомного наполнения body
+      bodyHtmlSnippet: '<div id="root"></div>' // наполняем дополнительно body
+    }),
+    new CleanWebpackPlugin(['build']), // удаляет папку build
+    new webpack.NamedModulesPlugin(), // make it easier to see which dependencies are being patched
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: { // настройки для HMR
+    contentBase: './build', // откуда брать данные для HMR
+    hot: true, // включаем HMR
+
+    noInfo: true, // убираю все консоль логи, кроме timings
+
+    // inline: false,
+    // colors: true,
+    // progress: true, // отображает прогресс сборки бандла
+  },
+  resolve: {
+    extensions: ['.jsx', '.js', '.json'], // автоматом подставляет данные расширения
+  }
 };
