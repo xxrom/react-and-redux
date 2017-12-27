d38856232cb640cf014fc8d50315eae3a28bd752
Nikita Chernyshov <xpom55@mail.ru>
2017-12-27 13:40:22 +0300

    HMR is Working

---
+++ src/App/App.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    console.log('APP component 2 ');
+    return <h1>Hello App HELLO NIKITA!!!</h1>;
+  }
+}
